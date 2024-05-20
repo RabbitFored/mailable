366ebcb9d512f162b0b611cf43ef763fa6d7847c
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2024-05-20 12:02:30 +0000

    have some custard

--- web.py
+++ web.py
@@ -1,6 +1,9 @@
-from quart import Quart, send_file, render_template
+from quart import Quart, send_file, render_template, request
 import requests
 from utils import strip_script_tags
+import json
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+import mailparser
 
 app = Quart(__name__, template_folder='web')
 
@@ -32,6 +35,30 @@ async def pages(id):
     content = req.text
     nojs = strip_script_tags(content)
     return await render_template("inbox.html" , content = nojs )
+
+
+@app.route('/cust', methods=['POST'])
+def reciever():
+
+  mailbytes = request.get_data()
+  mail = mailparser.parse_from_bytes(mailbytes)
+  multipart_data = MultipartEncoder(
+    fields={
+      "data":json.dumps({
+    "from" : mail.from_,
+    "to" : mail.to,
+    "cc" : mail.cc,
+    "bcc" : mail.bcc,
+    "subject" : mail.subject,
+    "body" : mail.body,
+    "text" : mail.text_plain,
+    "html" : mail.text_html,
+    "reply_to" : mail.reply_to,
+    "message_id" : mail.message_id
+    })}
+  )
+  requests.post("https://mail.bruva.co/secretmessages",data=multipart_data,headers={'Content-Type': multipart_data.content_type})
+  return 'Hello, World!'
 
 def run():
   app.run(host="0.0.0.0", port=8080)
